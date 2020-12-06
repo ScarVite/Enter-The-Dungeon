87e5a64b0167806688c4eb97048d94bc297d0a12
ScarVite <43887376+ScarVite@users.noreply.github.com>
2020-12-06 21:31:38 +0100

    Completely Reworked Networking

--- Enter The Dungeon/src/enterTheDungeon/api/Networking.java
+++ Enter The Dungeon/src/enterTheDungeon/api/Networking.java
@@ -24,280 +24,248 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import enterTheDungeon.game.Oberklassen.User;
+
 
 public class Networking {
-	
-	public static String						BaseUrl		= "https://api.scarvite.de/etd";
-	
-	// Ich weis, das ich nen boolean in nen string umwandel und dann wieder zurï¿½ck, aber ohne das gehts irgendwie nicht
-	
-	private final static CloseableHttpClient	httpClient	= HttpClients.createDefault();
-	
+
+	private static String userToken = new User().getToken();
+	private static String baseUrl = "https://api.scarvite.de/etd";
+
+	// Ich weis, das ich nen boolean in nen string umwandel und dann wieder
+	// zurück, aber ohne das gehts irgendwie nicht
+
+	private final static CloseableHttpClient httpClient = HttpClients.createDefault();
+
 	public static boolean validatekey(String key) {
-		HttpGet request = new HttpGet(BaseUrl + "/validatekey?key=" + key);
-		try(CloseableHttpResponse response = httpClient.execute(request)) {
+		HttpGet request = new HttpGet(baseUrl + "/validatekey?key=" + key);
+		request.setHeader("Authorization", userToken);
+		try (CloseableHttpResponse response = httpClient.execute(request)) {
 			System.out.println(response.getStatusLine().toString());
 			HttpEntity entity = response.getEntity();
 			Header headers = entity.getContentType();
 			System.out.println(headers);
-			if(entity != null) {
+			if (entity != null) {
 				String result;
 				try {
-					System.out.println("hier");
 					result = EntityUtils.toString(entity);
-					if(Boolean.parseBoolean(result) == true) {
+					if (Boolean.parseBoolean(result) == true) {
 						// In Speicherfile activated = true setzen
 					}
 					return Boolean.parseBoolean(result);
-				}
-				catch (ParseException e) {
+				} catch (ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}
-				catch (IOException e) {
+				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
-		}
-		catch (ClientProtocolException e1) {
+
+		} catch (ClientProtocolException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
-		catch (IOException e1) {
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		System.exit(1);
 		return false;
 	}
-	
+
 	public static boolean updateLeaderboard(String username, int score) {
 		System.out.println("hier");
-		HttpPost post = new HttpPost("https://api.scarvite.de/etd/updateleaderboard");
+		HttpPost post = new HttpPost(baseUrl + "/updateleaderboard");
 		/*
-		 * List<NameValuePair> urlParameters = new ArrayList<>(); urlParameters.add(new BasicNameValuePair("username", username)); urlParameters.add(new BasicNameValuePair("score",
-		 * Integer.toString(score))); urlParameters.add(new BasicNameValuePair("token", "33317-200-10-5-5-992")); try { post.setEntity(new UrlEncodedFormEntity(urlParameters)); } catch
-		 * (UnsupportedEncodingException e2) { // TODO Auto-generated catch block e2.printStackTrace(); }
+		 * List<NameValuePair> urlParameters = new ArrayList<>(); urlParameters.add(new
+		 * BasicNameValuePair("username", username)); urlParameters.add(new
+		 * BasicNameValuePair("score", Integer.toString(score))); urlParameters.add(new
+		 * BasicNameValuePair("token", "33317-200-10-5-5-992")); try {
+		 * post.setEntity(new UrlEncodedFormEntity(urlParameters)); } catch
+		 * (UnsupportedEncodingException e2) { // TODO Auto-generated catch block
+		 * e2.printStackTrace(); }
 		 */
-		try(CloseableHttpClient httpClient = HttpClients.createDefault(); CloseableHttpResponse response = httpClient.execute(post)) {
+		try (CloseableHttpClient httpClient = HttpClients.createDefault();
+				CloseableHttpResponse response = httpClient.execute(post)) {
 			System.out.println(response.getStatusLine().toString());
 			HttpEntity entity = response.getEntity();
 			Header headers = entity.getContentType();
 			System.out.println(headers);
-			if(entity != null) {
+			if (entity != null) {
 				try {
-					JSONArray jsonarray = new JSONArray(EntityUtils.toString(entity));
-					boolean result = jsonarray.getBoolean(0);
-					if(result == false) {
-						Popup.error(jsonarray.getString(1), "Error");
-						return result;
-					}
-					return result;
-				}
-				catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (IOException e) {
+					return true;
+				} catch (ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
-		}
-		catch (ClientProtocolException e1) {
+
+		} catch (ClientProtocolException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
-		catch (IOException e1) {
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		System.exit(1);
 		return false;
 	}
-	
+
 	public static JSONArray getLeaderboard() {
-		HttpGet request = new HttpGet(BaseUrl + "/getleaderboard");
-		try(CloseableHttpResponse response = httpClient.execute(request)) {
+		HttpGet request = new HttpGet(baseUrl + "/getleaderboard");
+		try (CloseableHttpResponse response = httpClient.execute(request)) {
 			System.out.println(response.getStatusLine().toString());
 			HttpEntity entity = response.getEntity();
 			Header headers = entity.getContentType();
 			System.out.println(headers);
-			if(entity != null) {
+			if (entity != null) {
 				try {
-					JSONArray jsonarray = new JSONArray(EntityUtils.toString(entity));
-					System.out.println(jsonarray.getJSONObject(0));
+					JSONParser parser = new JSONParser();
+					JSONArray jsonarray = (JSONArray) parser.parse(EntityUtils.toString(entity));
+					System.out.println(jsonarray.get(0));
 					return jsonarray;
-				}
-				catch (ParseException e) {
+				} catch (org.json.simple.parser.ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}
-				catch (IOException e) {
+				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
-		}
-		catch (ClientProtocolException e1) {
+
+		} catch (ClientProtocolException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
-		catch (IOException e1) {
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		System.exit(1);
 		return null;
 	}
-	
-	public static String addUser(String Email, String Username, String Password) {
-		String HashedPassword = " ";
+
+	public static void addUser(String email, String username, String password) {
+		String hashedPassword = " ";
 		try {
-			HashedPassword = hashPW(Password);
-		}
-		catch (NoSuchAlgorithmException e2) {
+			hashedPassword = hashPW(password);
+		} catch (NoSuchAlgorithmException e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 			System.exit(2);
 		}
-		if(HashedPassword == " ") {
+		if (hashedPassword == " ") {
 			System.exit(2);
 		}
-		HttpPost post = new HttpPost(BaseUrl + "/addUser");
+		HttpPost post = new HttpPost(baseUrl + "/addUser");
 		List<NameValuePair> urlParameters = new ArrayList<>();
-		urlParameters.add(new BasicNameValuePair("username", Username));
-		urlParameters.add(new BasicNameValuePair("password", HashedPassword));
-		urlParameters.add(new BasicNameValuePair("email", Email));
+		urlParameters.add(new BasicNameValuePair("username", username));
+		urlParameters.add(new BasicNameValuePair("password", hashedPassword));
+		urlParameters.add(new BasicNameValuePair("email", email));
 		try {
 			post.setEntity(new UrlEncodedFormEntity(urlParameters));
-		}
-		catch (UnsupportedEncodingException e2) {
+		} catch (UnsupportedEncodingException e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
-		try(CloseableHttpClient httpClient = HttpClients.createDefault(); CloseableHttpResponse response = httpClient.execute(post)) {
+		try (CloseableHttpClient httpClient = HttpClients.createDefault();
+				CloseableHttpResponse response = httpClient.execute(post)) {
 			System.out.println(response.getStatusLine().toString());
 			HttpEntity entity = response.getEntity();
 			Header headers = entity.getContentType();
 			System.out.println(headers);
-			if(entity != null) {
+			if (entity != null) {
 				try {
-					JSONArray jsonarray = new JSONArray(EntityUtils.toString(entity));
-					String answ = jsonarray.getString(1);
-					System.out.println(answ);
-					return answ;
-				}
-				catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (IOException e) {
+					JSONParser parser = new JSONParser();
+					JSONObject UserObj;
+					UserObj = (JSONObject) parser.parse(EntityUtils.toString(entity));
+					User.setUser(UserObj);
+				} catch (ParseException | org.json.simple.parser.ParseException | IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
-		}
-		catch (ClientProtocolException e1) {
+
+		} catch (ClientProtocolException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
-		catch (IOException e1) {
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		System.exit(1);
-		return null;
 	}
-	
-	public static JSONObject login(String Email, String Password) {
-		String HashedPassword = " ";
+
+	public static void login(String email, String password) {
+		String hashedPassword = " ";
 		try {
-			HashedPassword = hashPW(Password);
-		}
-		catch (NoSuchAlgorithmException e2) {
+			hashedPassword = hashPW(password);
+		} catch (NoSuchAlgorithmException e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
-		if(HashedPassword == " ") {
+		if (hashedPassword == " ") {
 			System.exit(2);
 		}
-		HttpPost post = new HttpPost(BaseUrl + "/login");
+		HttpPost post = new HttpPost(baseUrl + "/login");
 		List<NameValuePair> urlParameters = new ArrayList<>();
-		urlParameters.add(new BasicNameValuePair("email", Email));
-		urlParameters.add(new BasicNameValuePair("password", HashedPassword));
+		urlParameters.add(new BasicNameValuePair("email", email));
+		urlParameters.add(new BasicNameValuePair("password", hashedPassword));
 		try {
 			post.setEntity(new UrlEncodedFormEntity(urlParameters));
-		}
-		catch (UnsupportedEncodingException e2) {
+		} catch (UnsupportedEncodingException e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
-		try(CloseableHttpClient httpClient = HttpClients.createDefault(); CloseableHttpResponse response = httpClient.execute(post)) {
+		try (CloseableHttpClient httpClient = HttpClients.createDefault();
+				CloseableHttpResponse response = httpClient.execute(post)) {
 			System.out.println(response.getStatusLine().toString());
 			HttpEntity entity = response.getEntity();
 			Header headers = entity.getContentType();
 			System.out.println(headers);
-			if(entity != null) {
+			if (entity != null) {
 				try {
-					// jsonarray[0] = entweder true oder false, bei true is an [1] ein object bei false null
-					JSONArray jsonarray = new JSONArray(EntityUtils.toString(entity));
-					boolean found = jsonarray.getBoolean(0);
-					if(found == true) {
-						return jsonarray.getJSONObject(1);
-					}
-					else {
-						String message = jsonarray.getString(1);
-						Popup.error(message, "Error");
-						return null;
-					}
-					// System.out.println(jsonarray.getJSONObject(1));
-				}
-				catch (ParseException e) {
+					JSONParser parser = new JSONParser();
+					 JSONObject UserObj = (JSONObject) parser.parse(EntityUtils.toString(entity));
+					 User.setUser(UserObj);
+					
+				} catch (org.json.simple.parser.ParseException | ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}
-				catch (IOException e) {
+				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
-		}
-		catch (ClientProtocolException e1) {
+
+		} catch (ClientProtocolException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
-		catch (IOException e1) {
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		System.exit(1);
-		return null;
 	}
-	
-	public static String GenerateToken(int Score) {
+
+	public static String GenerateToken(int score) {
 		String token = " ";
 		Random random = new Random();
-		
+
 		boolean fertig = true;
-		
-		while(fertig) {
-			
+
+		while (fertig) {
+
 			int value = random.nextInt(17);
 			int value1 = random.nextInt(17);
 			int value2 = random.nextInt(17);
 			int value3 = random.nextInt(17);
 			int value4 = random.nextInt(17);
-			
-			if(value + value1 + value2 + value3 + value4 == 17) {
+
+			if (value + value1 + value2 + value3 + value4 == 17) {
 				fertig = false;
 				token = token + Integer.toString(value);
 				token = token + Integer.toString(value1);
@@ -306,41 +274,41 @@ public class Networking {
 				token = token + Integer.toString(value4);
 				token = token + "-";
 				System.out.println(token);
-				
+
 			}
-			
+
 		}
-		
+
 		return token;
 	}
-	
-	protected static String Tokengen(int Ziel, int lenght, int max) {
-		
+
+	protected static String Tokengen(int ziel, int lenght, int max) {
+
 		int a = 0;
-		while(a != Ziel) {
+		while (a != ziel) {
 			Random random = new Random();
 			int[] value = new int[lenght];
-			
-			for(int i = 0; i < value.length; i++) {
+
+			for (int i = 0; i < value.length; i++) {
 				value[i] = random.nextInt(max);
 			}
 			String strArray[] = new String[value.length];
-			
-			for(int i = 0; i < value.length; i++)
+
+			for (int i = 0; i < value.length; i++)
 				strArray[i] = String.valueOf(value[i]);
 			a = 0;
-			for(int i = 0; i < value.length; i++) {
+			for (int i = 0; i < value.length; i++) {
 				a = a + value[i];
-				
+
 			}
-			
-			if(a == Ziel) {
+
+			if (a == ziel) {
 				return Arrays.toString(strArray);
 			}
 		}
-		return "Error";
+		return null;
 	}
-	
+
 	private static String hashPW(String password) throws NoSuchAlgorithmException {
 		MessageDigest md = MessageDigest.getInstance("SHA-256");
 		md.update(password.getBytes(StandardCharsets.UTF_8));
@@ -349,5 +317,5 @@ public class Networking {
 		System.out.println(hashedPw);
 		return hashedPw;
 	}
-	
+
 }
